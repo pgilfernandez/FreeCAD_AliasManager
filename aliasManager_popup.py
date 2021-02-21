42e88245f948bfa4c6d99458cf16fea4e55231b0
spanner888 <spanner888@ocz-4-deb.WORKGROUP>
2021-02-21 16:47:33 +1100

    python2 to python3 changed xrange to range

--- aliasManager_popup.py
+++ aliasManager_popup.py
@@ -151,7 +151,7 @@ class p():
 
                 def char_range(c1, c2):
                     """Generates the characters from `c1` to `c2`, inclusive."""
-                    for c in xrange(ord(c1), ord(c2)+1):
+                    for c in range(ord(c1), ord(c2)+1):
                         yield str.capitalize(chr(c))
 
                 fam_range = []
